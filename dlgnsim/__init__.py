ab3db73ca5c76a641d1831d933655f1cec53656e
dcrombie <d.crombie@campus.lmu.de>
2020-01-21 18:14:55 +0100

    Remove stupid __init__ imports

--- dlgnsim/__init__.py
+++ dlgnsim/__init__.py
@@ -1,9 +1,1 @@
-import numpy as np
-import quantities as pq
-import matplotlib.pyplot as plt
-
-import pylgn
-import pylgn.kernels.spatial as spl
-import pylgn.kernels.temporal as tpl
-
 from . import util
